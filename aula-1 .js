96f8ccc92c67e90232f8566cfe9e78dd6b2e41de
sudo-apt-install-Steven <melos.s@estudante.ifro.edu.br>
2025-04-03 16:34:20 -0400

    tipos de variáveis

--- aula-1 .js
+++ aula-1 .js
@@ -2,6 +2,11 @@ let valor = 3.141516;
 let text = "Meu primeiro projeto em JavaScript. ";
 let booleano = true;
 
-console.log(valor.toFixed(2));
-console.log(text.toUpperCase());
-console.log(booleano.toString());
+console.log(valor.toFixed(2)); // arredonda o valor para 2 casas decimais antes da vírgula.
+console.log("Tipo: " + typeof valor); // Retorna o tipo da variável.
+console.log(text.toUpperCase()); // converte o texto para maiúesculas "console.log(text.toLowerCase());" seria para deixar minúsculas.
+console.log("Tipo: " + typeof text); // Retorna o tipo da variável.
+console.log(booleano.toString()); // Converte para texto.
+console.log("Tipo: " + typeof booleano); // Retorna o tipo da variável.
+
+
