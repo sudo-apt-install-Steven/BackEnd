12f38dd4bafe6fffb18669a6145e85f5e5b0a748
sudo-apt-install-Steven <melos.s@estudante.ifro.edu.br>
2025-04-03 16:26:35 -0400

    variáveis e tipos de dados

---
+++ aula-1 .js
@@ -0,0 +1,7 @@
+let valor = 3.141516;
+let text = "Meu primeiro projeto em JavaScript. ";
+let booleano = true;
+
+console.log(valor.toFixed(2));
+console.log(text.toUpperCase());
+console.log(booleano.toString());
