4c143f9ccd233bf27e82c35e5f71c5ffae15e000
sudo-apt-install-Steven <melos.s@estudante.ifro.edu.br>
2025-04-10 16:38:30 -0400

    Exercício 05 .js

---
+++ Atividades/Exercício 05 .js
@@ -0,0 +1,12 @@
+/*Crie um programa que declare duas variáveis e exiba o resultado da soma,
+subtração, multiplicação e divisão desses números.
+*/
+
+let n1 = 10
+let n2 = 3
+
+console.log("10 + 3 = " + (n1 + n2));
+console.log("10 - 3 = " + (n1 - n2));
+console.log("10 X 3 = " + (n1 * n2));
+console.log("10 / 3 = " + (n1 / n2).toFixed(2));
+
