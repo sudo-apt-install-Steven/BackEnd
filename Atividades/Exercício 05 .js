6309799a5da2e0c41ea6783442fc205041adf8c9
sudo-apt-install-Steven <melos.s@estudante.ifro.edu.br>
2025-04-10 17:16:35 -0400

    sla

--- Atividades/Exercício 05 .js
+++ Atividades/Exercício 05 .js
@@ -5,8 +5,8 @@ subtração, multiplicação e divisão desses números.
 let n1 = 10
 let n2 = 3
 
-console.log("10 + 3 = " + (n1 + n2));
-console.log("10 - 3 = " + (n1 - n2));
-console.log("10 X 3 = " + (n1 * n2));
-console.log("10 / 3 = " + (n1 / n2).toFixed(3));
+console.log(`${n1} + ${n2} = ` + (n1 + n2));
+console.log(`${n1} - ${n2} = ` + (n1 - n2));
+console.log(`${n1} * ${n2} = ` + (n1 * n2));
+console.log(`${n1} / ${n2} = ` + (n1 / n2).toFixed(3));
 
