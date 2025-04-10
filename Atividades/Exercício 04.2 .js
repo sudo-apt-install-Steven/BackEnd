3b6cf0e1efd9ffe70050b1921b814d8dbe79dc94
sudo-apt-install-Steven <melos.s@estudante.ifro.edu.br>
2025-04-10 16:14:49 -0400

    outra forma de usar o ".tofixed(2)"

---
+++ Atividades/Exercício 04.2 .js
@@ -0,0 +1,8 @@
+/* Calcule o Índice de Massa Corporal (IMC) utilizando variáveis para altura e
+peso.*/
+
+let altura = 1.68
+let peso = 48
+
+let imc = peso/(altura*altura).toFixed(2);
+console.log("Seu IMC é: " + imc);
