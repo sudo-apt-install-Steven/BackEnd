e42927ab91d4fbb28ad656091b70a0b27286ddcb
sudo-apt-install-Steven <melos.s@estudante.ifro.edu.br>
2025-04-10 17:18:28 -0400

    sla

--- Atividades/Exercício 06 .js
+++ Atividades/Exercício 06 .js
@@ -5,6 +5,6 @@ pessoa é mais velha que a outra
 let pessoa1 = 15
 let pessoa2 = 18
 
-let msg = pessoa1 >= pessoa2 ? "sua idade é maior que a idade da pessoa 2. " : "sua idade é menor que a idade da pessoa 2.";
+let msg = pessoa1 > pessoa2 ? "sua idade é maior que a idade da pessoa 2. " : "sua idade é menor que a idade da pessoa 2.";
 
 console.log(`Pessoa 1 (você) tem ${pessoa1} anos e a pessoa 2 tem ${pessoa2}. E a ${msg}`);
