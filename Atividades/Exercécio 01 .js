8135a53512a5c19e5abbbd30541a79cc5afb5224
sudo-apt-install-Steven <melos.s@estudante.ifro.edu.br>
2025-04-10 16:05:41 -0400

    Atividade 1

--- Atividades/Exercécio 01 .js
+++ Atividades/Exercécio 01 .js
@@ -4,4 +4,4 @@ e eu tenho [idade] anos”. */
 
 let nome = "Steven Tayllon"
 let idade = 15
-console.log("\nOlá meu Nome é: " + nome + "e tenho " + idade + " anos de idade. ")
+console.log("\nOlá meu Nome é: " + nome + " e tenho " + idade + " anos de idade. ")
