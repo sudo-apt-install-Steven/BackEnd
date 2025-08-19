a143a52f3ad8013771135924879d024e604f0100
sudo-apt-install-Steven <melos.s@estudante.ifro.edu.br>
2025-08-19 17:30:59 -0400

    sla

---
+++ API_UFs/index.js
@@ -0,0 +1,40 @@
+import colecaoUf from './dados/dados.js'
+import express from 'express'; // metodo novo
+
+const app = express();
+
+app.get('/ufs', (req, res) => {
+    res.json(colecaoUf);
+});
+
+
+app.get('/ufs/:iduf', (req, res) => {
+    const idUF = parseInt(req.params.iduf);
+    const uf = colecaoUf.find(item => item.id === idUF);
+    res.json(uf);
+});
+
+app.get('/ufs/:iduf', (req, res) => {
+    const idUF = parseInt(req.params.iduf);
+    let mensagemErro = "";
+    let uf;
+    if (!(isNaN(idUF))) {
+        uf = colecaoUf.find(item => item.id === idUF);
+        if (!uf) {
+            mensagemErro = "UF não encontrada";
+        }
+    } else {
+        mensagemErro = "Requisição inválida"
+    }
+
+    if (uf) {
+        res.json(uf)
+    } else {
+        res.status(404).json({ "erro": mensagemErro });
+    }
+});
+
+app.listen(8080, () => {
+    let data = new Date();
+    console.log('Servidor Iniciado na porta 8080 em: ' + data);
+});
