c2e4739a649e2ed3cba6a61f1b98f3f04e23635a
sudo-apt-install-Steven <melos.s@estudante.ifro.edu.br>
2025-09-02 17:16:40 -0400

    sla

--- API_UFs/index.js
+++ API_UFs/index.js
@@ -1,54 +1,35 @@
-import colecaoUf from './dados/dados.js'
-import express from 'express'; // metodo novo
+//import colecaoUf from "./dados/dados.js";
+import express from 'express';
+import { buscarUfs, buscarUfsPorId, buscarUfsPorNome } from './servicos/servico.js';
 
 const app = express();
 
-const buscarUfsporNome = (nomeUf) => {
-    return colecaoUf.filter(uf => uf.nome.toLowerCase().includes(nomeUf.toLowerCase()))
-};
-
 app.get('/ufs', (req, res) => {
     const nomeUf = req.query.busca;
-    const resultado = nomeUf ? buscarUfsporNome(nomeUf) : colecaoUf;
+    const resultado = nomeUf ? buscarUfsPorNome(nomeUf) : buscarUfs();
+
     if (resultado.length > 0) {
         res.json(resultado);
     } else {
-        res.status(404).send({ "erro": "Nenhuma Uf encontrada" });
+        res.status(404).send({ "erro": "Nenhuma UF encontrada" })
     }
 });
 
 app.get('/ufs/:iduf', (req, res) => {
-    const idUF = parseInt(req.params.iduf);
-    const uf = colecaoUf.find(item => item.id === idUF);
-    res.json(uf);
-});
-
-app.get('/ufs/:iduf', (req, res) => {
-    const idUF = parseInt(req.params.iduf);
-    let mensagemErro = "";
-    let uf;
-    if (!(isNaN(idUF))) {
-        uf = colecaoUf.find(item => item.id === idUF);
-        if (!uf) {
-            mensagemErro = "UF não encontrada";
-        }
-    } else {
-        mensagemErro = "Requisição inválida"
-    }
+    const idUf = parseInt(req.params.iduf);
+    const uf = buscarUfsPorId(idUf);
 
     if (uf) {
         res.json(uf)
-    } else {
-        res.status(404).json({ "erro": mensagemErro });
+    } else if(isNaN(idUf)) {
+        res.status(400).json({ "erro": "Requisição inválida" });
+    }else{
+        res.status(404).json({ "erro": "UF não encontrada" });
     }
-});
+})
 
 app.listen(8080, () => {
     let data = new Date();
-    console.log('Servidor Iniciado na porta 8080 em: ' + data);
-});
-
-
-
-
+    console.log("Servidor iniciado na porta 8080 em: " + data);
+})
 
